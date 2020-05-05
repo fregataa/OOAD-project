8061d927346d1a3eab6067e1c5f3f680f0d7f824
Sang Hun <oper6909@gmail.com>
2020-05-06 01:08:20 +0900

    JUnit after-build check

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,6 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello JUnit. cacerts check");
+        System.out.println("Hello JUnit. YEAHHHHHHHHHHH!!! ");
     }
 }
