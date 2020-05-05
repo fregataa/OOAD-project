189f39244c72cb50c96c6ccd98e341faf6c18dd4
Sang Hun <oper6909@gmail.com>
2020-05-05 23:34:06 +0900

    item setting changes

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,6 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello JUnit. wtf");
+        System.out.println("Hello JUnit. fuck");
     }
 }
