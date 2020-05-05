7938e7d5f89ac72366e812c0c50f51338b818ec0
Sang Hun <oper6909@gmail.com>
2020-05-05 23:07:16 +0900

    first commit with JUnit

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,11 @@
 public class Calculator {
+
+    public int add(){
+        int result;
+        int a=1, b=5;
+        return a+b;
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello Jira commit test");
     }
---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void main() {
+        assertEquals(0, 0);
+    }
+
+    @Test
+    public void add() {
+        Calculator c = new Calculator();
+        int r = c.add();
+        assertEquals(6, r);
+    }
+}
