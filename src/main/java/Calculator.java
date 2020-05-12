dddc01a709c3b58c786f66c9c1d108f5942fbe75
Sang Hun <oper6909@gmail.com>
2020-05-12 21:12:39 +0900

    static analysis error test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -8,7 +8,7 @@ public class Calculator {
 
     public static void main(String[] args) {
         int a=0;
-        int b=1;
+        int b;
         if(b==1){
             System.out.println("branch in");
         } else {
