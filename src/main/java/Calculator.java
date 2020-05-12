4e9c73372447c259c8b27aa661bd9511bf173219
Sang Hun <oper6909@gmail.com>
2020-05-12 18:13:45 +0900

    sonarqube second try again

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,6 +7,6 @@ public class Calculator {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello Sonarqube. Second try ");
+        System.out.println("Hello Sonarqube. Second try again ");
     }
 }
