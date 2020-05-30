ccf05357f1906643f661f1d50ae45e341265342d
박재영 <jyoung-man>
2020-05-30 16:17:24 +0900

    add class TurnipPrice

---
+++ src/main/java/Timer.java
@@ -0,0 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class Timer {
+    private LocalDateTime timerTime;
+    private int maxCursor;
+    private int maxValueofCorsor[];
+    private int maxPage;
+
+    public void startTimer(LocalDateTime timeValue) {
+
+    }
+    public LocalDateTime getTimerValue() {
+
+    }
+
+    public void saveTimer(LocalDateTime timeValue) {
+
+    }
+
+    public void pauseTimer() {
+
+    }
+    public void resetTimer() {
+
+    }
+}
---
+++ src/main/java/TurnipPrice.java
@@ -0,0 +1,26 @@
+import java.time.LocalDateTime;
+
+public class TurnipPrice {
+
+    private LocalDateTime date;
+    private int price[];
+    private Boolean isInputed[];
+    private Boolean isHighestDate[];
+    private int maxCursor;
+    private int maxValueofCorsor[];
+    private int maxPage;
+
+    public int inputPrice(int currentPage) {
+
+    }
+    public void savePrice(int priceValue){
+
+    }
+    public void resetPrice() {
+
+    }
+    public int nextPrice(int currentPage) {
+
+    }
+
+}
