13e9d66b5b9c9d76f42f228e21c8f3446a1db319
Hyeon Cho <mbin96@gmail.com>
2020-05-30 16:10:40 +0900

    add class Turnipcalc

---
+++ src/main/java/TurnipCalc.java
@@ -0,0 +1,12 @@
+import java.time.*;
+public class TurnipCalc {
+    public localDate halfday;
+    public int[] price = new int[13];
+    public int calcPrice(){
+
+    }
+    public void setHighestPrice(){
+        
+    }
+
+}
