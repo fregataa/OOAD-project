b43b22cbe82a0aaf16210f31b59923eff7762e3f
박재영 <jyoung-man>
2020-06-02 15:37:56 +0900

    add Alarm method

--- src/main/java/Alarm.java
+++ src/main/java/Alarm.java
@@ -1,19 +1,30 @@
 import java.time.LocalTime;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Alarm{
-    private LocalDateTime alarmTime;
+    private ZonedDateTime alarmTime;
     private boolean isActivated;
     private int maxCursor;
-    private int[] maxValueOfCursor = new int[];
+    private int[] maxValueOfCursor = new int[]{};
     private int maxPage;
 
-    public LocalDateTime alarmTime getAlarmValue(){ reuturn this.alarmTime;}
-    public void saveAlarmTime(){}
-    public void activateAlarm(){}
-    public void deactivateAlarm(){}
-    public int nextAlarm(){}
+    public ZonedDateTime getAlarmValue(){
+        return this.alarmTime;
+    }
+    public void saveAlarmTime(ZonedDateTime newAlarm){    //파라미터가 필요할 것 같다.
+        this.alarmTime = newAlarm;
+    }
+    public void activateAlarm(){
+        this.isActivated = true;
+    }
+    public void deactivateAlarm(){
+        this.isActivated = false;
+    }
+    public int nextAlarm(){ //하나의 알람 객체는 하나의 정보만 가지기 때문에 이건 Controller의 메소드로 구형해야 할 것 같다.
+        return 0;
+    }
 }
